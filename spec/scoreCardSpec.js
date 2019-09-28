a83f6cd6b854e1ab49c35bbfefc553893b263246
Mike <mjwalshe@gmail.com>
2019-09-28 19:33:57 +0100

    Pass test for game of ones

--- spec/scoreCardSpec.js
+++ spec/scoreCardSpec.js
@@ -10,4 +10,11 @@ describe("ScoreCard", function() {
     }
     expect(scoreCard.getScore()).toEqual(0);
   })
+
+  it("scores 20 for game of ones", function() {
+    for (i=0; i<20; i++) {
+      scoreCard.addThrow(1);
+    }
+    expect(scoreCard.getScore()).toEqual(20);
+  })
 });
--- src/scoreCard.js
+++ src/scoreCard.js
@@ -2,12 +2,19 @@
 
 var ScoreCard = function() {
   this._score = 0;
+  this._throws = [];
+  this._throwNumber = 0;
 }
 
 ScoreCard.prototype.addThrow = function(pinsDown) {
-  this._score += pinsDown;
+  this._throws.push(pinsDown);
+  this._score = this._throws.reduce(summer);
+  
+  function summer(total, num) {
+    return total + num;
+  }
 }
 
 ScoreCard.prototype.getScore = function() {
-  return 0;
-};
+  return this._score;
+}
