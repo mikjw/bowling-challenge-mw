f315077e4716e3f78c45367c268721e517d02569
Mike <mjwalshe@gmail.com>
2019-09-29 21:28:02 +0100

    Pass test for two spares

--- spec/scoreCardSpec.js
+++ spec/scoreCardSpec.js
@@ -10,15 +10,22 @@ describe("ScoreCard", function() {
     expect(scoreCard.getScore(pinsDown)).toEqual(0);
   })
 
-  it("scores 20 for game of ones", function() {
+  it("scores 20 for 1 on every throw", function() {
     var pinsDown = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
 
     expect(scoreCard.getScore(pinsDown)).toEqual(20);
   })
 
-  it("scores 29 with spare in first frame", function() {
+  it("scores 29 with spare in 1st frame", function() {
     var pinsDown = [1,9,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
 
     expect(scoreCard.getScore(pinsDown)).toEqual(29);
   })
+
+  it("scores 38 with spares in 1st and 5th", function() {
+    var pinsDown = [1,9,1,1,1,1,1,1,9,1,1,1,1,1,1,1,1,1,1,1];
+
+    expect(scoreCard.getScore(pinsDown)).toEqual(38);
+  })
+
 });
